79276f42ac3c6114e0117e47831fda2003ab65c3
Zhongmiao Li <mars.leezm@gmail.com>
2014-10-05 19:12:05 +0100

    Change interface

--- src/ad_counter.erl
+++ src/ad_counter.erl
@@ -1,24 +1,22 @@
 -module(ad_counter).
--export([run/1, consume_ad/3]).
+-export([run/1, consume_ad/4]).
 
 
 run(Counter) ->
     Key = stupid, 
-    spawn(ad_counter, consume_ad, [Key, 0, 5]),
-    spawn(ad_counter, consume_ad, [Key, 0, 5]),
+    spawn(ad_counter, consume_ad, [Key, 0, 5, chinese]),
+    spawn(ad_counter, consume_ad, [Key, 0, 5, belgian]),
     %spawn(ad_counter, consume_ad, [Key, 0, 15]),
     derflow_api:threshold_read(Key, riak_dt_gcounter, Counter).
 
-consume_ad(_Key, Limit, Limit) ->
-    ok;
-consume_ad(Key, Count, Limit) ->
+consume_ad(Key, Count, Limit, Actor) ->
     Rand = random:uniform(10),
     timer:sleep(Rand),
     case Count of
         Limit ->
             ok;
         _ ->
-            derflow_api:update(Key, riak_dt_gcounter, {increment, chinese}),
-            consume_ad(Key, Count+1, Limit)
+            derflow_api:update(Key, riak_dt_gcounter, {increment, Actor}),
+            consume_ad(Key, Count+1, Limit, Actor)
     end.
     
